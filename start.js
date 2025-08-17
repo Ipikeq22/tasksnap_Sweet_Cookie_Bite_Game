876f3c451270587fce0b044cc41dff8ee0f801e8
Ipikeq22 <ipike388@gmail.com>
2025-08-18 00:57:27 +0800

    Update start.js

--- start.js
+++ start.js
@@ -1,8 +1,5 @@
 function startGame() {
-    playBackgroundMusic(); // 開始播放背景音樂
-    setTimeout(() => {
-        window.location.href = 'game.html';
-    }, 500); // 稍微延遲跳轉，讓音樂開始播放
+    window.location.href = 'game.html';
 }
 
 function goHome() {
